2b5ad8409741291df525229f25e8a12e1c779c2d
popup0221 <popup0221@naver.com>
2024-07-02 23:50:01 +0900

    chore: 프로젝트 탭 변경

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,17 @@
 const projectsData = [
+  // {
+  //   title: 'A Search Engine',
+  //   description: `What if you could look up any information in the world? Webpages, images, videos
+  //   and more. Google has many features to help you find exactly what you're looking
+  //   for.`,
+  //   imgSrc: '/static/images/google.png',
+  //   href: 'https://www.google.com',
+  // },
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'ball-again',
+    description: `k-pop으로 배우는 한글단어 학습 웹 서비스`,
+    imgSrc: '/static/images/ball-again/main.jpg',
+    href: 'https://ball-again.vercel.app/',
   },
 ]
 
