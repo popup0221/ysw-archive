e4e28093caa1cacf831c201b8d6a8b01c7c7a760
popup0221 <popup0221@naver.com>
2024-07-03 23:49:51 +0900

    chore: 프로젝튼 주석제거

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,12 +1,4 @@
 const projectsData = [
-  // {
-  //   title: 'A Search Engine',
-  //   description: `What if you could look up any information in the world? Webpages, images, videos
-  //   and more. Google has many features to help you find exactly what you're looking
-  //   for.`,
-  //   imgSrc: '/static/images/google.png',
-  //   href: 'https://www.google.com',
-  // },
   {
     title: 'ball-again',
     description: `k-pop으로 배우는 한글단어 학습 웹 서비스`,
